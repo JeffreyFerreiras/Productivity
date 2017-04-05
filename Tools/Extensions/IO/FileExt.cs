38f865107072946a1520b412933e889738f55529
Ferreiras <XBBND77@ams.bnymellon.net>
2017-04-05 09:34:26 -0400

    added fileext method

---
+++ Tools/Extensions/IO/FileExt.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Tools.Extensions.IO
+{
+    public static class FileExt
+    {
+    }
+}
