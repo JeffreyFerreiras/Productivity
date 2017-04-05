c17ef2d46c31551abca3680178e75939b464cd16
Ferreiras <XBBND77@ams.bnymellon.net>
2017-04-05 09:40:03 -0400

    CreateDirectory update

--- Tools/Extensions/IO/FileExt.cs
+++ Tools/Extensions/IO/FileExt.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.IO;
 
 namespace Tools.Extensions.IO
 {
     public static class FileExt
     {
+        public static bool CreateDirectory(string path)
+        {
+            FileAttributes attr = File.GetAttributes(path);
+
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                Directory.CreateDirectory(path);
+            else
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+            return Directory.Exists(path);
+        }
     }
 }
