947105235bb4602fa5391518a33ed80fbfe26218
BotJeff <Jeffrey.E.Ferreiras@gmail.com>
2017-06-08 18:32:16 -0400

    Updated unit tests

---
+++ Tools.Test/Extensions/CollectionsExTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NSubstitute;
+using Tools.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tools.Test.Extensions
+{
+    [TestClass]
+    public class CollectionsExTests
+    {
+        [TestMethod]
+        public void ToDictionary_ConvertObject_IDictionary()
+        {
+            var item = new
+            {
+                a = "Hellon world!",
+                b = 5,
+                c = 11.11,
+                d = new int [] {1,2,3,4,5},
+                e = "Hello world!".ToCharArray()
+            };
+
+            IDictionary<string, object> itemDictionary;
+            itemDictionary = item.ToDictionary();
+
+            Assert.IsTrue(itemDictionary.Count > 0);
+        }
+    }
+}
--- Tools/Extensions/CollectionsEx.cs
+++ Tools/Extensions/CollectionsEx.cs
@@ -5,20 +5,23 @@ using System.Reflection;
 
 namespace Tools.Extensions
 {
+    using Exceptions;
+
     public static class CollectionsEx
     {
         /// <summary>
         /// Converts an object into a dictioanry of key value pair.
-        /// The keys are strings matching the property names and the values match that properties value.
-        /// If a key exists it will be overwritten.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <typeparam name="TValue"></typeparam>
         /// <param name="item"></param>
         /// <returns></returns>
-        public static IDictionary<string, TValue> ToDictionary<T, TValue>(this T item)
+        
+        public static IDictionary<string, dynamic> ToDictionary<T>(this T item)
         {
-            IDictionary<string, TValue> dict = new Dictionary<string, TValue>();
+            Guard.ThrowIfInvalidArgs(item);
+
+            IDictionary<string, dynamic> dict = new Dictionary<string, dynamic>();
 
             foreach (var p in typeof(T).GetProperties())
             {
