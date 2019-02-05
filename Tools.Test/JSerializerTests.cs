d87b644569e95768b65b6f33f71d173bda992be7
Jeffrey <jeffrey.e.ferreiras@gmail.com>
2019-02-05 11:24:56 -0500

    added json serializer

---
+++ Tools.Test/JSerializerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+
+namespace Tools.Test
+{
+    [TestFixture]
+    public class JSerializerTests
+    {
+        [Test]
+        public void ToJson_ValidInput_ReturnsJson()
+        {
+            object item = new
+            {
+                Name = "Jeffrey",
+                Last = "Ferreiras",
+                Age =  28
+            };
+
+
+            string result = JSerializer.ToJson(item);
+
+            Assert.IsTrue(result.Length > 0);
+        }
+    }
+}
---
+++ Tools/JSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Tools
+{
+    public static class JSerializer
+    {
+        /// <summary>
+        /// Write object to JSON source type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string ToJson<T>(T source)
+        {
+            //Create a stream to serialize the object to
+            MemoryStream ms = new MemoryStream();
+
+            // Serializer the User object to the stream
+            var ser = new DataContractJsonSerializer(typeof(T));
+
+            ser.WriteObject(ms, source);
+           
+            byte[] json = ms.ToArray();
+
+            ms.Close();
+
+            return Encoding.UTF8.GetString(json, 0, json.Length);
+        }
+    }
+}
